72583e2b59f2e72b719bfba4df30f4747223c93e
MoonLightt01 <byrman.slavik@gmail.com>
2023-05-30 14:28:16 +0300

    add hw 2

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("**************************\n****\t  Визитка\t  ****\n**** Name: Слава\t  ****\n");
+        System.out.print("**** Skype: surname28 ****\n**************************");
+    }
+}
